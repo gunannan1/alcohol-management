10a6e6c20b0f4a2aedc2be43857c9c28c9359eca
Nannan Gu <396426364@qq.com>
2019-04-03 01:00:42 +1100

    update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,42 +3,42 @@
     <el-card class="login-form-layout">
       <el-form autoComplete="on"
                :model="loginForm"
-               :rules="loginRules"
                ref="loginForm"
                label-position="left">
         <div style="text-align: center">
           <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EFF"></svg-icon>
         </div>
-        <h2 class="login-title color-main">mall-admin-web</h2>
+        <h2 class="login-title color-main">Alcohol Project Management System</h2>
+
         <el-form-item prop="username">
           <el-input name="username"
                     type="text"
                     v-model="loginForm.username"
                     autoComplete="on"
-                    placeholder="请输入用户名">
+                    placeholder="Username" readonly
+                    onfocus="this.removeAttribute('readonly');" >
           <span slot="prefix">
             <svg-icon icon-class="user" class="color-main"></svg-icon>
           </span>
           </el-input>
         </el-form-item>
+
         <el-form-item prop="password">
           <el-input name="password"
                     :type="pwdType"
                     @keyup.enter.native="handleLogin"
                     v-model="loginForm.password"
                     autoComplete="on"
-                    placeholder="请输入密码">
+                    placeholder="Password">
           <span slot="prefix">
             <svg-icon icon-class="password" class="color-main"></svg-icon>
           </span>
-            <span slot="suffix" @click="showPwd">
             <svg-icon icon-class="eye" class="color-main"></svg-icon>
-          </span>
           </el-input>
         </el-form-item>
         <el-form-item style="margin-bottom: 60px">
           <el-button style="width: 100%" type="primary" :loading="loading" @click.native.prevent="handleLogin">
-            登录
+            Login
           </el-button>
         </el-form-item>
       </el-form>
@@ -48,34 +48,15 @@
 </template>
 
 <script>
-  import {isvalidUsername} from '@/utils/validate';
   import login_center_bg from '@/assets/images/login_center_bg.png'
 
   export default {
     name: 'login',
     data() {
-      const validateUsername = (rule, value, callback) => {
-        if (!isvalidUsername(value)) {
-          callback(new Error('请输入正确的用户名'))
-        } else {
-          callback()
-        }
-      };
-      const validatePass = (rule, value, callback) => {
-        if (value.length < 3) {
-          callback(new Error('密码不能小于3位'))
-        } else {
-          callback()
-        }
-      };
       return {
         loginForm: {
-          username: 'admin',
-          password: '123456'
-        },
-        loginRules: {
-          username: [{required: true, trigger: 'blur', validator: validateUsername}],
-          password: [{required: true, trigger: 'blur', validator: validatePass}]
+          username: '',
+          password: ''
         },
         loading: false,
         pwdType: 'password',
@@ -83,13 +64,6 @@
       }
     },
     methods: {
-      showPwd() {
-        if (this.pwdType === 'password') {
-          this.pwdType = ''
-        } else {
-          this.pwdType = 'password'
-        }
-      },
       handleLogin() {
         this.$refs.loginForm.validate(valid => {
           if (valid) {
@@ -101,13 +75,14 @@
               this.loading = false
             })
           } else {
-            console.log('参数验证不合法！');
+            console.log('invalid parameter!');
             return false
           }
         })
       }
     }
   }
+
 </script>
 
 <style scoped>
