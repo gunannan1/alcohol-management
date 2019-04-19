4f7ab5fe4402bac05d74a5db0ba70f5672b83278
Nannan Gu <396426364@qq.com>
2019-04-19 18:10:09 +1000

    update

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -29,7 +29,7 @@ export default {
       let matched = this.$route.matched.filter(item => item.name)
       const first = matched[0]
       if (first && first.name !== 'home') {
-        matched = [{ path: '/home', meta: { title: '首页' }}].concat(matched)
+        matched = [{ path: '/home', meta: { title: 'HomePage' }}].concat(matched)
       }
       this.levelList = matched
     }
