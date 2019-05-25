21b1fb28727019774a43cd6df5410925c8c15a21
Nannan Gu <396426364@qq.com>
2019-05-26 00:14:46 +1000

    update

--- src/views/404.vue
+++ src/views/404.vue
@@ -6,10 +6,9 @@
       </el-col>
       <el-col :span="12">
         <div style="margin-left: 100px;margin-top: 60px">
-          <h1 class="color-main">OOPS!</h1>
-          <h2 style="color: #606266">很抱歉，页面它不小心迷路了！</h2>
-          <div style="color:#909399;font-size: 14px">请检查您输入的网址是否正确，请点击以下按钮返回主页或者发送错误报告</div>
-          <el-button style="margin-top: 20px" type="primary" round @click="handleGoMain">返回首页</el-button>
+          <h1 class="color-main">404 not found!</h1>
+          <div style="color:#909399;font-size: 14px">Please check the correctness of the url </div>
+          <el-button style="margin-top: 20px" type="primary" round @click="handleGoMain">return to home page</el-button>
         </div>
       </el-col>
     </div>
@@ -17,7 +16,7 @@
 </template>
 
 <script>
-  import img_404 from '@/assets/images/gif_404.gif';
+  import img_404 from '@/assets/images/404.png';
 
   export default {
     name: 'wrongPage',
